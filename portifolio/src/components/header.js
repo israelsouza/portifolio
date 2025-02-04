b018b0f9f629d0c36fd06b88539e1023d26606ed
Israel de Souza <israellimas@hotmail.com>
2025-02-04 20:40:13 -0300

    feat - add componente cabecalho

---
+++ portifolio/src/components/header.js
@@ -0,0 +1,35 @@
+import style from './Header.module.css'
+
+function Header() {
+    return (
+        <header className={style.header }>
+            <div className={`${style.header__content} ${style.container}`}>
+                <h2 className={style.header__title}>
+                    &lt; IsRael / &gt;
+                </h2>
+
+                <nav className={style.header__menu}>
+                    <ul className={style.header__list}>
+                        <li className={style.header__listItem}>
+                            <a href="" target="_self" className={style.header__link}>
+                                Inicio
+                            </a>
+                        </li>
+                        <li className={style.header__listItem}>
+                            <a href="" target="_self" className={style.header__link}>
+                                Sobre
+                            </a>
+                        </li>
+                        <li className={style.header__listItem}>
+                            <a href="" target="_self" className={style.header__link}>
+                                Projetos
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
