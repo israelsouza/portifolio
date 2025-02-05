1698a17b7bb0d164358d27ac53b3779276900f52
Israel de Souza <israellimas@hotmail.com>
2025-02-04 21:56:23 -0300

    feat: add componente imagem de fundo

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -1,3 +1,4 @@
+import FundoPrincipal from './components/fundoPrincipal';
 import Header from './components/header';
 import './App.css';
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Header/>
+      <FundoPrincipal />
     </>
   );
 }
